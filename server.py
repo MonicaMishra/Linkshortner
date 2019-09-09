7a8cc79406bb6a4abbe039e78f8df8fbc3f6f781
Monica Mishra <32046396+MonicaMishra@users.noreply.github.com>
2019-09-09 17:15:39 -0400

    Update server.py

--- server.py
+++ server.py
@@ -7,24 +7,24 @@ import string
 from urllib.parse import urlparse
 import LinkShortener
 
-host = 'https://localhost:5000/'
+host = 'https://localhost:5000/' ## to run on local host
 
 app = Flask(__name__)
 CORS(app)
 
-short_url = LinkShortener()
+short_url = LinkShortener() ## create an object from the class
 
-@app.route('/')
+@app.route('/') ## first link pointer
 def hello():
     return send_file('./index.html')
 
-@app.route('/url', methods=['POST'])
+@app.route('/url', methods=['POST']) ## need to read the url from user and later shorten from shorten_url in class LinkShortener
 def parse_url():
     url = request.form.get('url')
     my_short_url = short_url.shorten_url(url)
     return my_short_url
 
-@app.route('/decode/<_id>')
+@app.route('/decode/<_id>') ## read the encoded url and open a new window to show that the decoded url is the same as the one provided originally
 def decode_url(_id):
     my_long_url = short_url.base62_decoder(_id)
     html = """
@@ -41,5 +41,5 @@ def decode_url(_id):
     """.format(my_long_url)
     return html
 
-if __name__ == '__main__':
+if __name__ == '__main__': ## run the main function
     app.run()
